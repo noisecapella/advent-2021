7e74c7757bb38d0d8648dbc329b14982977b0f78
George Schneeloch <noisecapella@gmail.com>
2021-12-30 09:01:42 -0500

    WIP

--- day20.py
+++ day20.py
@@ -12,65 +12,68 @@ def read_input(filename):
         grid = np.array([[c == "#" for c in line.strip()] for line in f.readlines()], dtype='bool')
         return algorithm, grid
 
-def calc_pixel(image, x, y, times, depth, algorithm, expand_x, expand_y):
-
+def calc_pixels(image, times, algorithm):
+    extra_x = 0  # just one side
+    extra_y = 0
+    inc_x = 12
+    inc_y = 12
+    
     @cache
-    def _calc_pixel(x, y, times, depth, expand_x, expand_y):
-        if depth == 0:
-            if not (0 <= x < image.shape[0]) or not (0 <= y < image.shape[1]):
-                return False
-            else:
-                return image[x, y]
-        
+    def _calc_pixel(x, y, depth):
         binary = ''
+        outer_bounds_x = image.shape[0] + (depth * 2 * inc_x) + extra_x
+        outer_bounds_y = image.shape[1] + (depth * 2 * inc_y) + extra_y
+        inner_bounds_x = outer_bounds_x - (2 * inc_x)
+        inner_bounds_y = outer_bounds_y - (2 * inc_y)
         for dx in (-1, 0, 1):
             for dy in (-1, 0, 1):
-                inner_x = x + dx - (times - depth) * (expand_x // 2)
-                inner_y = y + dy - (times - depth) * (expand_y // 2)
-                bounds_x = image.shape[0] + (times * expand_x)
-                bounds_y = image.shape[1] + (times * expand_y)
+                inner_x = x + dx - inc_x
+                inner_y = y + dy - inc_y
     
-                if not (0 <= inner_x < bounds_x) or not (0 <= inner_y < bounds_y):
-                    binary += '0'
-                else:
-                    value = _calc_pixel(x - (expand_x // 2) + dx, y - (expand_y // 2) + dy, times, depth - 1, expand_x, expand_y)
+                if 0 <= inner_x < inner_bounds_x and 0 <= inner_y < inner_bounds_y:
+                    if depth == 1:
+                        value = image[inner_x, inner_y]
+                    else:
+                        value = _calc_pixel(inner_x, inner_y, depth - 1)
                     binary += '1' if value else '0'
+                else:
+                    binary += "0"
         return algorithm[int(binary, 2)] == "#"
 
-    return _calc_pixel(x, y, times, depth, expand_x, expand_y)
-    
+    new_image = np.zeros(shape=(image.shape[0] + (times * 2 * inc_x) + extra_x, image.shape[1] + (times * 2 * inc_y) + extra_y), dtype='bool')
+    for _x in range(new_image.shape[0]):
+        for _y in range(new_image.shape[1]):
+            new_image[_x + extra_x, _y + extra_y] = _calc_pixel(_x, _y, times)
+
+    return new_image
         
     
 def enhance(image, algorithm, times):
-    extra_x = 4
-    extra_y = 4
-    new_image = np.zeros(shape=(image.shape[0] + (extra_x * times), image.shape[1] + (extra_y * times)), dtype='bool')
-    for x in range(new_image.shape[0]):
-        for y in range(new_image.shape[1]):
-            new_image[x, y] = calc_pixel(image, x, y, times, times, algorithm, extra_x, extra_y)
+    return calc_pixels(image, times, algorithm)
 
-    for x in range(new_image.shape[0]):
-        new_image[x, 0] = False
-        new_image[x, new_image.shape[0] - 1] = False
-    for y in range(new_image.shape[1]):
-        new_image[0, y] = False
-        new_image[new_image.shape[1] - 1, y] = False
-    return new_image
 
+def make_grid(image):
+    return "".join(["".join([ "#" if c else "." for c in line]) + "\n" for line in image])
 
 def print_grid(image):
-    print("".join(["".join([ "#" if c else "." for c in line]) + "\n" for line in image]))
-    
+    print(make_grid(image))
+
+
 def main():
     algorithm, image = read_input("day20.txt")
+
     #print_grid(image)
     #enhanced_1 = enhance(image, algorithm, 1)
     #print_grid(enhanced_1)
+
     enhanced_2 = enhance(image, algorithm, 2)
-    #print_grid(enhanced_2)
+    print_grid(enhanced_2)
     print("part 1", np.count_nonzero(enhanced_2))
-    enhanced_50 = enhance(image, algorithm, 50)
-    print("part 2", np.count_nonzero(enhanced_50))
+    #enhanced_50 = enhance(image, algorithm, 50)
+    #print_grid(enhanced_50)
+    #with open("out.txt", "w") as f:
+    #    f.write(make_grid(enhanced_50))
+    #print("part 2", np.count_nonzero(enhanced_50))
 
 if __name__ == "__main__":
     main()
